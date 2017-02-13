ad09d09a8f0695e054c7f64189b28a9eba6bdf59
bykim <logaegae@gmail.com>
2017-02-13 18:12:09 +0900

    02-13

--- routes/front/main.js
+++ routes/front/main.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var exRouter = express.Router();
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
 
 exRouter.use("/", router);
 
 router.all("/index.ejs", function(req, res, next){
+    var listJson = [];
+    var list = fs.readdirSync('./views/','utf-8')
+    list.forEach(function(e,i){
+        if(e.indexOf('.ejs') != -1) {
+            var name = e.split('.ejs')[0];
+            listJson.push({file:name});
+        }
+    })
+    res.locals.listJson = listJson;
+
     res.locals.title="Study AngularJs with NodeJs";
     next();
 });
